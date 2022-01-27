77e5b71ade136e8f05221ee52ec91efcd03fba18
Shane Mulligan <mullikine@gmail.com>
2022-01-27 20:34:10 +1300

    1643268850

--- src/khala/core.clj
+++ src/khala/core.clj
@@ -1,6 +1,8 @@
 (ns khala.core
   (:require [org.httpkit.server :refer [run-server]]
-            [clj-time.core :as t])
+            [clj-time.core :as t]
+            [compojure.core :refer :all]
+            [compojure.route :as route])
   (:gen-class))
 
 (defn app [req]
@@ -8,6 +10,14 @@
    :headers {"Content-Type" "text/html"}
    :body    (str (t/time-now))})
 
+(defroutes app
+  (GET "/" [] "<h1>Khala</h1>")
+  (GET "/prompt" [] (get-time))
+  (route/not-found "<h1>Khala service not found</h1>"))
+
 (defn -main [& args]
-  (run-server app {:port 8080})
+  (let [port (Integer/parseInt (or (System/getenv "PORT") "9837"))] ;(5) 
+    (server/run-server app {:port port})
+    (println (str "Running Khala at http:/127.0.0.1:" port "/")))
+
   (println "Server started on port 8080"))
