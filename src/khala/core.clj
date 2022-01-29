fb3bc915a259900ffca8636688e83f63d844c744
Shane Mulligan <mullikine@gmail.com>
2022-01-29 13:50:48 +1300

    Sat 29 Jan 2022 13:50:48

--- src/khala/core.clj
+++ src/khala/core.clj
@@ -209,5 +209,6 @@
 
 (defn -main [& args]
   (let [port (Integer/parseInt (or (System/getenv "PORT") "9837"))] ;(5)
-    (server/run-server app {:port port})
+    (server/run-server app {:port port
+                            :max-body 8388608})
     (println (str "Running Khala at http:/127.0.0.1:" port "/"))))
