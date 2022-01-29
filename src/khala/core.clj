feaeba6547d9a27577edfc088ba9f7a78955e482
Shane Mulligan <mullikine@gmail.com>
2022-01-29 13:45:56 +1300

    Sat 29 Jan 2022 13:45:56

--- src/khala/core.clj
+++ src/khala/core.clj
@@ -186,7 +186,8 @@
     ;; "http://127.0.0.1:800/prompt"
     {:body
      ;; (string-input-stream (json/write-str {:fun "xyz" :args "xyz"}))
-     (string-input-stream (json/write-str {:username "xyz" :password "xyz"}))
+     ;; (string-input-stream (json/write-str {:username "xyz" :password "xyz"}))
+     (json/write-str {:username "xyz" :password "xyz"})
      :accept :json
      :headers {"Content-Type" "application/json; charset=utf-8"}
      ;; :form-params {"q" "foo, bar"}
