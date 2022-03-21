d22d5cfb980219657074d9a53197d8feea10d505
Shane Mulligan <mullikine@gmail.com>
2022-03-21 18:51:56 +1300

    21.03.22

--- src/khala/dev.clj
+++ src/khala/dev.clj
@@ -1,6 +1,7 @@
 (ns khala.dev
   (:require [khala.core :refer :all]
-            [khala.utils :as u]))
+            [khala.utils :as u]
+            [khala.mount :as mount]))
 
 (use '[clojure.java.shell :only [sh]])
 
@@ -8,6 +9,9 @@
   (start-khala 9897)
   (sh "firefox" "http:/127.0.0.1:9897")
   (stop-khala))
+
+(defn mount-pensieve []
+  (mount/pensieve "pensieve" (u/expand-home "$HOME/pensieve")))
 
 (defn unmount-pensieve []
   (sh "umount" (u/expand-home  "$HOME/pensieve")))
